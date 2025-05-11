33f5ed852895fe4e0d6af3ad484d9b776695e263
asoeb <a.soeborg-skau@hotmail.dk>
2025-05-11 11:35:16 +0200

    Initial commit

---
+++ src/test/java/com/bilabbonement/bilabonnement/BilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilabbonement.bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
