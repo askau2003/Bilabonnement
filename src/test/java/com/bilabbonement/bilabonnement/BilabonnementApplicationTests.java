9f6552c889b1673df9c779166934284fda5b33df
asoeb <a.soeborg-skau@hotmail.dk>
2025-05-15 09:12:11 +0200

    Sidste til Dashboard

--- src/test/java/com/bilabbonement/bilabonnement/BilabonnementApplicationTests.java
+++ src/test/java/com/bilabbonement/bilabonnement/BilabonnementApplicationTests.java
@@ -1,7 +1,14 @@
 package com.bilabbonement.bilabonnement;
 
+import com.bilabbonement.bilabonnement.Model.OmsaetningMaaned;
+import com.bilabbonement.bilabonnement.Service.LejekontraktService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BilabonnementApplicationTests {
@@ -10,4 +17,17 @@ class BilabonnementApplicationTests {
     void contextLoads() {
     }
 
+
+    @Test
+    void omsaetningsMaanedTest() {
+
+        List<OmsaetningMaaned> omsaetningList = new ArrayList<>();
+        omsaetningList.add(new OmsaetningMaaned("EURO",100));
+        omsaetningList.add(new OmsaetningMaaned("DKK",1000));
+
+        LejekontraktService lejekontraktService = new LejekontraktService();
+        Double result = lejekontraktService.omsaetningMaaned();
+
+        assertEquals(100 * 7.5 + 1000, result);
+    }
 }
