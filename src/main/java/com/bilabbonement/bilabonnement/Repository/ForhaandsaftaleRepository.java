a363bfec755cc79e42d5aac8258158d58e8ae3f7
asoeb <a.soeborg-skau@hotmail.dk>
2025-05-15 16:09:36 +0200

    Opret Forhåndsaftale 2

--- src/main/java/com/bilabbonement/bilabonnement/Repository/ForhaandsaftaleRepository.java
+++ src/main/java/com/bilabbonement/bilabonnement/Repository/ForhaandsaftaleRepository.java
@@ -13,8 +13,8 @@ public class ForhaandsaftaleRepository {
 
     public void opretForhaandsaftale(Forhaandsaftale forhaandsaftale) {
         String sql = "INSERT INTO forhaandsaftale " +
-                "(aftale_id,pris,betalt,afhentnings_adresse,lejer_id,vognnummer) " +
-                "VALUES (?,?,?,?,?,?)";
+                "(pris,betalt,afhentnings_adresse,lejer_id,vognnummer) " +
+                "VALUES (?,?,?,?,?)";
         template.update(sql, forhaandsaftale.getAftale_id(),forhaandsaftale.getPris(),forhaandsaftale.isBetalt(),
                 forhaandsaftale.getAfhentnings_adresse(),forhaandsaftale.getLejer_id(), forhaandsaftale.getVognnummer());
     }
